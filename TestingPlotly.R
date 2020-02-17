3adb3cacd83e7faa4185ced54a453e3257878fca
David Grinder <grinderd@gmail.com>
2020-02-17 17:09:55 -0500

    Initial commit of notes and presentation

---
+++ TestingPlotly.R
@@ -0,0 +1,22 @@
+# 
+# Purpose: This code is meant to be a starting point with plotly.  Generate some plots off of some example data
+# 
+# Date: 02/15/2020
+# 
+# version    Date      Descriptions
+# _______    _____     ____________
+# 0.1        2/15/2020 Initial Development
+#################
+  
+  
+#install.packages("plotly")
+
+#Load appropriate libraries
+library(plotly)
+
+#box plot
+p <- plot_ly(midwest, x= ~percollege, color = ~state, type = "box")
+
+p <- plot_ly(midwest, x= ~percollege, color = percamerindian~state, type = "box")
+
+p
